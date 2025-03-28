07488aa0a6b2efe3eb07926ee5ddbcbf6d61b5b4
desphindj <desphindennis@gmail.com>
2025-03-28 17:08:57 +0530

    Add files via upload

---
+++ total/level2/two/Two.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Two extends Component {
+  render() {
+    return (
+      <div className='bor'> 
+        Hello from a class component!
+      </div>
+    );
+  }
+}
+
+export default Two;
